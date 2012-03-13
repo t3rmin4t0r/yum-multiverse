d6da3b35ddea4009abf176a270702ed1f887ae94
Gopal V <gopalv@php.net>
2012-03-13 23:12:10 +0530

    yum-multiverse first-code

---
+++ multiverse.py
@@ -0,0 +1,22 @@
+# ---
+# I should live here:
+# /usr/lib/yum-plugins/multiverse.py
+
+import re
+from yum import config
+from yum.plugins import TYPE_CORE
+
+requires_api_version = '2.4'
+plugin_type = TYPE_CORE
+
+def config_hook(conduit):
+    config.RepoConf.multiversions = config.ListOption()
+
+def postreposetup_hook(conduit):
+     base = conduit._base
+     for repo in conduit.getRepos().listEnabled():
+         patterns = [pkg.replace("*", '.*') for pkg in repo.multiversions]
+         regex = re.compile("^(" + ( ")|(".join(patterns) ) + ")$")
+         pkgs = [pkg.name for pkg in base.pkgSack.returnPackages(repo.id) if regex.match(pkg.name)]
+         base.conf.installonlypkgs.extend(pkgs)
+         #print base.conf.installonlypkgs
